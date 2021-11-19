94502ccc5daf591e3e1678c98b3cb152eda1f918
Marshall T. Rose <mrose17@gmail.com>
2021-11-19 17:48:28 -0500

    Revert jquery.js

--- calendar/jquery.js
+++ calendar/jquery.js
@@ -5813,10 +5813,7 @@ function remove( elem, selector, keepData ) {
 
 jQuery.extend( {
 	htmlPrefilter: function( html ) {
-/*
 		return html.replace( rxhtmlTag, "<$1></$2>" );
-*/
-                return html;
 	},
 
 	clone: function( elem, dataAndEvents, deepDataAndEvents ) {
